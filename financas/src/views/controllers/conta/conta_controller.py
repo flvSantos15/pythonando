0507e80e239e2a20272814452fb29a18fe23a395
flvsantos15 <flvsantos300@gmail.com>
2025-03-24 22:52:23 -0300

    feat: add list accounts function

--- financas/src/views/controllers/conta/conta_controller.py
+++ financas/src/views/controllers/conta/conta_controller.py
@@ -1,8 +1,9 @@
 from sqlmodel import Session, select  # type: ignore
 from src.model.configs.base import engine
+from src.model.entities.bancos import Bancos
 from src.model.entities.contas import Conta
 
-#  Parei em 1:00:06
+#  Parei em 1:03:39
 
 def create_account(conta: Conta):
   with Session(engine) as session:
@@ -16,3 +17,9 @@ def create_account(conta: Conta):
     session.add(conta) # Adiciona no banco
     session.commit() # Salva no banco
     return conta
+
+def list_accounts():
+  with Session(engine) as session:
+    statement = select(Conta)
+    results = session.exec(statement).all()
+  return results
