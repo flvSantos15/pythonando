8e0df045b052dbd9d11cda2dee5fb9961941931d
flvsantos15 <flvsantos300@gmail.com>
2025-04-02 23:32:39 -0300

    chore: add payload to create studant function

--- mobile/app.py
+++ mobile/app.py
@@ -2,9 +2,9 @@ import flet as ft
 import requests
 
 # URL base da API – ajuste conforme necessário
-API_BASE_URL = "http://localhost:8000/api/treino"
+API_BASE_URL = "http://localhost:8000/api"
 
-# Parei em 30:10
+# Parei em 36:37
 
 def main(page: ft.Page):
     page.title = "Exemplo"
@@ -16,7 +16,19 @@ def main(page: ft.Page):
     data_nascimento_field = ft.TextField(label="Data de Nascimento (YYYY-MM-DD)")
 
     def criar_aluno(e):
-        print("Criando aluno...")
+        payload = {
+            "nome": nome_field.value,
+            "email": email_field.value,
+            "faixa": faixa_field.value,
+            "data_nascimento": data_nascimento_field.value,
+        }
+
+        response = requests.post(f"{API_BASE_URL}/alunos/", json=payload)
+        if response.status_code == 201:
+            create_result.value = "Aluno criado com sucesso!"
+        else:
+            create_result.value = f"Erro ao criar aluno: {response.text}"
+
 
     create_result = ft.Text()
     create_button = ft.ElevatedButton("Criar Aluno", on_click=criar_aluno)
