8795b6707e29feacc485ffca6993ec0da1668453
flvsantos15 <flvsantos300@gmail.com>
2025-04-05 14:17:58 -0300

    feat: add progress student def

--- mobile/app.py
+++ mobile/app.py
@@ -4,7 +4,7 @@ import requests
 # URL base da API – ajuste conforme necessário
 API_BASE_URL = "http://localhost:8000/api"
 
-# Parei em 1:01:00
+# Parei em 2:09:41
 
 def main(page: ft.Page):
     page.title = "Exemplo"
@@ -91,13 +91,35 @@ def main(page: ft.Page):
             "qtd": int(qtd_field.value),
         }
 
-        response = requests.post(f"{API_BASE_URL}/aulas_realizadas/", json=payload)
+        response = requests.post(f"{API_BASE_URL}/aula_realizada/", json=payload)
         if response.status_code == 200:
-            aula_result.value = f"Aula realizada com sucesso"
+            aula_result.value = f"Sucesso: {response.json()}"
         else:
             aula_result.value = f"Erro: {response.text}"
-
         page.update()
+
+    email_progress_field = ft.TextField(label="Email do Aluno")
+    progress_result = ft.Text()
+
+    def consultar_progresso_click(e):
+        email = email_progress_field.value
+        response = requests.get(f"{API_BASE_URL}/progresso_aluno/", params={'email_aluno': email})
+
+        if response.status_code == 200:
+            progress = response.json()
+            progress_result.value = (
+                f"Nome: {progress.get('nome')}\n"
+                f"Email: {progress.get('email')}\n"
+                f"Faixa: {progress.get('faixa')}\n"
+                f"Total de aulas concluidas: {progress.get('total_aulas')}\n"
+                f"Aulas necessarias para a proxima faixa: {progress.get('aulas_necessarias_para_proxima_faixa')}"
+            )
+        else:
+            progress_result.value = f"Erro: {response.text}"
+        page.update()
+
+    progress_button = ft.ElevatedButton("Consultar progresso", on_click=consultar_progresso_click)
+    progress_tab = ft.Column([ email_progress_field, progress_button, progress_result], scroll=True)
 
     aula_button = ft.ElevatedButton("Realizar Aula", on_click=marcar_aula_click)
     aula_tab = ft.Column([ student_email_field, qtd_field, aula_button, aula_result], scroll=True)
@@ -108,6 +130,7 @@ def main(page: ft.Page):
           ft.Tab(text="Criar Aluno", content=criar_aluno_tab),
           ft.Tab(text="Listar Alunos", content=list_students_tab),
           ft.Tab(text="Cadastrar Aula", content=aula_tab),
+          ft.Tab(text="Progresso do aluno", content=progress_tab),
         ]
     )
 
